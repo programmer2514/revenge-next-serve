50c44d84e681b514a923e791138a508288fbe738
Benjamin Pryor <43104632+programmer2514@users.noreply.github.com>
2026-07-06 01:44:10 +0000

    Heuristically move plugins to the appropriate stage folder

--- build.sh
+++ build.sh
@@ -15,7 +15,18 @@ git clone https://github.com/revenge-mod/revenge-bundle-next.git
 git clone https://github.com/PalmDevs/revenge-next-plugins.git
 
 # Move plugins to the appropriate folder
-mv revenge-next-plugins/plugins/* revenge-bundle-next/src/plugins/start
+for PLUGIN_DIR in revenge-next-plugins/plugins/* ; do
+  PLUGIN_ROOT=$(find "$PLUGIN_DIR" -name "index.*")
+  STAGE=$(
+    grep -Po "^\s*(async\s*)?(preInit|init|start)\(.*\)\s*\{\s*$" "$PLUGIN_ROOT" |\
+    grep -Po "^\s*(async\s*)?(preInit|init|start)\(" |\
+    grep -Po "(preInit|init|start)"
+  )
+  DEST_DIR="revenge-bundle-next/src/plugins/$STAGE"
+
+  mkdir -p "$DEST_DIR"
+  mv "$PLUGIN_DIR" "$DEST_DIR"
+done
 
 # Remove old plugins folder
 rm -rf revenge-next-plugins
